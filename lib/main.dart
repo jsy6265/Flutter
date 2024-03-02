50e082461dd513b5f1560c46afb94f527e792912
wjstmdduf <wjs6265@gmail.com>
2024-03-02 17:43:05 +0900

    Padding, Row, Culumn 실습

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,52 @@ void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello flutter"),
-        ),
-        body: Center(
-            child: Text("Hello world"),
+        backgroundColor: Color(0xFF181818), // 커스텀 색상 쓰는법 Color(0xFF + 색 코드)
+        // home의 배경색을 지정하는 위젯 타입의 파라미터, Colors.원하는 색.그림자
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                //Row의 MainAxis는 수평방향, CrossAixs는 수직 방향이다.
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    // Column의 MainAxis는 수직방향, CrossAixs는 수평방향
+                    children: [
+                      Text(
+                        "Hey, Selena",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        "Welcome back",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          //withOpacity로 투명도 조절
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
-
 }
