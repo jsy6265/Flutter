2488e6f009216697c0264c94ea3887791faa3825
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-03-11 21:25:17 +0900

    http_launch 실습, SharedPreferences 실습

--- toon_flex/lib/screens/detail_screen.dart
+++ toon_flex/lib/screens/detail_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:toon_flex/models/webtoon_detail_model.dart';
+import 'package:toon_flex/services/api_service.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class DetailScreen extends StatelessWidget {
+import '../models/webtoon_episode_model.dart';
+import '../widgets/episode_widget.dart';
+
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
   const DetailScreen({
@@ -10,6 +17,59 @@ class DetailScreen extends StatelessWidget {
     required this.thumb,
     required this.id,
   });
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+// 부모인 DetailScreen의 값에 접근하고 싶을 떈 widget.xxx로 접근 가능
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+  late SharedPreferences pref;
+  bool isLiked = false;
+
+  Future initPref() async {
+    // shared preferences 가져오기
+    pref = await SharedPreferences.getInstance();
+    // preferences에서 likedToons라는 키를 가진 문자열 리스트가 가져오기
+    final likedToons = pref.getStringList('likedToons');
+
+    // 최초 접속한 유저는 preferences에 아무 값도 없기 떄문에 likedToons가 있는지 검사
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id)) {
+        setState(() {
+          isLiked = true;
+        });
+      }
+    } else {
+      // 없으면 likedToons 생성
+      await pref.setStringList('likedToons', []);
+    }
+  }
+
+  onHeartTap() async {
+    final likedToons = pref.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+      await pref.setStringList('likedToons', likedToons);
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+    initPref();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,47 +84,105 @@ class DetailScreen extends StatelessWidget {
         // 음영 조절
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
+        actions: [
+          IconButton(
+            onPressed: onHeartTap,
+            icon: Icon(isLiked ? Icons.favorite : Icons.favorite_outline),
+          ),
+        ],
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 24,
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
             children: [
-              Hero(
-                tag: id,
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 15,
-                          offset: const Offset(10, 10),
-                          color: Colors.black.withOpacity(0.5),
-                        )
-                      ]),
-                  child: Image.network(
-                    thumb,
-                    headers: const {
-                      "User-Agent":
-                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                    },
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 15,
+                              offset: const Offset(10, 10),
+                              color: Colors.black.withOpacity(0.5),
+                            )
+                          ]),
+                      child: Image.network(
+                        widget.thumb,
+                        headers: const {
+                          "User-Agent":
+                              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                        },
+                      ),
+                    ),
                   ),
-                ),
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: webtoon,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                  return const Text('...');
+                },
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: episodes,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    // Listview는 여러 요소를 다루는데 최적화되어 있지만 구현하기 까다로운 부분이 있다
+                    // ListVide는 많은 양의 리스트를 다룰때만 추천
+                    return Column(
+                      children: [
+                        for (var episode in snapshot.data!)
+                          Episod(episode: episode, webtoonId: widget.id)
+                      ],
+                    );
+                  }
+                  return Container();
+                },
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
